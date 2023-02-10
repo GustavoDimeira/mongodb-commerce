9b0fefb2e3e1c64d088389cdda8abc4881657082
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 18:50:21 -0400

    Feat: sort and adding two new tags

---
+++ challenges/desafio23.js
@@ -0,0 +1,13 @@
+/*
+  Crie uma query que faça tanto a inserção dos valores combo e tasty no array tags de todos os sanduíches. Ordene os valores de tags em ordem alfabética ascendente.
+
+  Crie uma query que retorne o nome e tags de todos os documentos.
+*/
+
+db.produtos.updateMany({}, {
+  $push: { tags: { $each: [
+    "tasty", "combo",
+  ], $sort: 1 } },
+});
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
