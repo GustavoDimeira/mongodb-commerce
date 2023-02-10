c1ebf942be5a4d4bf6d3284c28bafbc5d7fedd36
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 18:14:40 -0400

    Feat: renive last ingridiente from Cheddar McMelt

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,13 +1,13 @@
 /*
-  Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+  Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt.
 
   Crie uma query que retorne o nome e ingredientes de todos os documentos.
 */
 
 db.produtos.updateOne({
-  nome: "Quarteirão com Queijo",
+  nome: "Cheddar McMelt",
 }, {
-  $pop: { ingredientes: -1 },
+  $pop: { ingredientes: 1 },
 });
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
