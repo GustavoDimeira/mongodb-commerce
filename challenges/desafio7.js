757bf37c322e14a655925c9aa015cc99fa73fa16
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 15:02:36 -0400

    Feat: remove documents

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,3 +1,1 @@
-// 7 - Retorne o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
-
 db.produtos.find({ $and: [{ vendidos: { $nin: [50] } }, { tags: { $exists: 0 } }] }, { _id: 0, vendidos: 1, nome: 1 } );
