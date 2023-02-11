88fa2bc68a989ad097b917554a16840ce66e9ee9
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 21:10:36 -0400

    Feat: remove a collun

---
+++ challenges/desafio30.js
@@ -0,0 +1,15 @@
+/*
+  Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+  Crie uma query que retorne o nome para todos os documentos e curtidas (exceto para Big Mac).
+*/
+
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $unset: {  curtidas: "" },
+});
+
+db.produtos.find({}, {
+  _id: 0, nome: 1, curtidas: 1,
+});
