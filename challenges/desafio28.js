bbf7f3608afa9044fbe30059b771ce378b4bcd89
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 21:06:17 -0400

    Fix: linter errors

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,5 +1,5 @@
 // Conte quantos produtos têm 4 ingredientes
 
 db.produtos.find(
-  { ingredientes: { $size: 4 } }
+  { ingredientes: { $size: 4 } },
 ).count();
--- challenges/desafio29.js
+++ challenges/desafio29.js
@@ -5,7 +5,7 @@
 */
 
 db.produtos.updateMany({}, {
-  $rename: { "descricao": "descricaoSite" },
+  $rename: { descricao: "descricaoSite" },
 });
 
 db.produtos.find({}, { _id: 0, nome: 1, descricaoSite: 1 });
