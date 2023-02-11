a3eb4faac8c2ebcd4a10e9810ce2ef00df5fd8f8
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 20:52:19 -0400

    Fix: regex sintax

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,5 +1,5 @@
 // Conte quantos produtos contém Mc no nome, sem considerar letras maiúsculas ou minúsculas
 
 db.produtos.find({
-  name: { $regex: /^mc^/i },
+  nome: { $regex: 'mc', $options: 'i' },
 }).count();
