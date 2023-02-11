88c29ad232ee756702e4f66ef774e11199c99bba
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-02-10 20:25:44 -0400

    Feat: Adding "muito sódio" as tag value

---
+++ challenges/desafio25.js
@@ -0,0 +1,14 @@
+/*
+  Crie uma query que faça a adição do valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+
+  Crie uma query que retorne o nome e tags de todos os documentos.
+*/
+
+db.produtos.updateMany({
+  "valoresNutricionais.2.percentual": { $gte: 40 },
+}, {
+  $push: { tags: "muito sódio",
+  },
+});
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
